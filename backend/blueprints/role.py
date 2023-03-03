fe0270ed716c158a0614f63702c172cfc5bf9999
Johannes Rønning <johannes.ronning@tietoevry.com>
2023-03-03 15:32:25 +0100

    added cross origin headers to roles/* endpoint

--- backend/blueprints/role.py
+++ backend/blueprints/role.py
@@ -5,11 +5,13 @@ from extensions import RESPONSE_CODES
 
 api = Blueprint("roles", __name__)
 
+@cross_origin()
 @api.get("/")
 def get_roles():
     roles = Role.query.all()
     return jsonify(Role.serialize_list(roles))
 
+@cross_origin()
 @api.get("/<int:id>")
 def get_role(id):
     roles = Role.query.filter(Role.id==id).first()
