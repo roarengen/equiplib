2f077d18f43cf27979f62f89d429329e06bbb767
Johannes Rønning <johannes.ronning@tietoevry.com>
2023-03-08 11:13:30 +0100

    made a app factory and setup for different states

--- backend/main.py
+++ backend/main.py
@@ -1,33 +1,46 @@
 from blueprints import api
+from enum import Enum, auto
 from flask import Flask
 from extensions import db, docs, cors, seed_database
 from extensions import sys_log_handler, file_handler
 import logging
 
+class LaunchArg(Enum):
+    TEST = auto()
+    DEV = auto()
+    PRD = auto()
+
 # intializing app
-app = Flask("equiplib")
-cors.init_app(app)
-# registering blueprint
-app.register_blueprint(api, url_prefix='/api/')
+def create_app(launch_arg : LaunchArg) -> Flask:
+    app = Flask("equiplib")
+    cors.init_app(app)
+    # registering blueprint
+    app.register_blueprint(api, url_prefix='/api/')
 
-# setting configs
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['CORS_HEADER'] = 'Content-Type'
+    # setting configs
+    if launch_arg == LaunchArg.DEV: 
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    elif launch_arg == LaunchArg.TEST:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['CORS_HEADER'] = 'Content-Type'
 
-# logging
-app.logger.handlers.clear()
-app.logger.addHandler(sys_log_handler)
-app.logger.addHandler(file_handler)
-app.logger.setLevel(logging.DEBUG)
+    # logging
+    app.logger.handlers.clear()
+    app.logger.addHandler(sys_log_handler)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.DEBUG)
 
-# initing extensions
-db.init_app(app)
-docs.init_app(app)
-app.app_context().push()
+    # initing extensions
+    db.init_app(app)
+    docs.init_app(app)
+    app.app_context().push()
+
+    return app
 
 
 #seed_database()
 
 if __name__ == "__main__":
+    app = create_app(LaunchArg.DEV)
     app.run("localhost", 8888, True)
