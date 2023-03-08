d108848951a17b0db8e6fd6e1c041f2aefdbf5dd
Johannes Rønning <johannes.ronning@tietoevry.com>
2023-03-08 11:15:23 +0100

    added data/init.py

---
+++ backend/data/__init__.py
@@ -0,0 +1,9 @@
+from data.equipment import Equipment
+from data.role import Role
+from data.user import User
+from data.template import Template
+from data.location import Location
+from data.organization import Organization
+from data.rent import Rent
+
+__all__ = [ "Equipment", "Role", "User", "Template", "Location", "Organization", "Rent"]
