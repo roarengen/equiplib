5fab69121b0464415a254f56087a1e6c90b18c4a
Johannes <johannesr@viken.no>
2023-03-12 00:17:31 +0100

    fixed typo causing erros in imports

--- backend/data/__init__.py
+++ backend/data/__init__.py
@@ -6,4 +6,4 @@ from data.location import Location
 from data.organization import Organization
 from data.rent import Rent
 
-__all__ = [ "Equipment", "Role", "Roles", "User", "Template", "Location", "Organization", "Rent"]
+__all__ = [ "Equipment", "Role", "ROLES", "User", "Template", "Location", "Organization", "Rent"]
