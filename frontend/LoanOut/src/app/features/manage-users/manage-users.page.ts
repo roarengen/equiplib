73648915f1924773a2a0ad9b0172ca7c91f5967e
Mads Thorstensen <mads.thorstensen@tietoevry.com>
2023-03-22 15:05:09 +0100

    Add user page.

--- frontend/LoanOut/src/app/features/manage-users/manage-users.page.ts
+++ frontend/LoanOut/src/app/features/manage-users/manage-users.page.ts
@@ -1,4 +1,8 @@
+import { AccountService } from './../../services/user.service';
+import { CustomHttpClient } from './../../helpers/auth/http-client';
 import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-manage-users',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageUsersPage implements OnInit {
 
-  constructor() { }
+  public username: string;
+  public firstname: string;
+  public lastname: string;
+  public email: string;
+  public phone: string;
+  public city: string;
+  public birthdate: Date;
+  public validthrudate: Date;
+
+  constructor(
+    private http: CustomHttpClient,
+    private accountService: AccountService,
+    private popoverController: PopoverController
+  ) { }
 
   ngOnInit() {
   }
 
-}
+  onSubmitNewUser() {
+    const data = {
+      organizationid: this.accountService.user.organizationid,
+      username: this.username,
+      firstname: this.firstname,
+      lastname: this.lastname,
+      email: this.email,
+      phone: this.phone,
+      city: this.city,
+      dateOfBirth: this.birthdate,
+      activeFromDate: new Date(),
+      activeToDate: this.validthrudate,
+    }
+      this.http.post(`${environment.apiUrl}/users`, data).subscribe(response => {
+        console.log(response)
+      })
+    }
+  }
