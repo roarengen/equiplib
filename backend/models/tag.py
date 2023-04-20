f4bad51aba25b27a02a8d10ffc034e571f2c547b
Johannes <johannes>
2023-04-20 09:47:34 +0200

    added color rgb validators fixed #110

--- backend/models/tag.py
+++ backend/models/tag.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 class TagBase(BaseModel):
     name: str
@@ -13,3 +13,11 @@ class Tag(TagBase):
 
 class TagCreate(TagBase):
     organizationid: int
+
+    @validator('color')
+    def color_hexargb(cls, v: str):
+        if len(v) != 7:
+            raise ValueError("expected length of color value is 7, got {len}" % len(v))
+        if v[0] != '#':
+            raise ValueError("missing mandatory hexacedimal color prefix '#'")
+        return v
