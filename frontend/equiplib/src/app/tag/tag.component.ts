0a883ebdc79e98e8e241588e2b0058921658998b
Johannes <johannes>
2023-04-20 10:20:59 +0200

    added tag component

---
+++ frontend/equiplib/src/app/tag/tag.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tag',
+  templateUrl: './tag.component.html',
+  styleUrls: ['./tag.component.scss'],
+})
+export class TagComponent  implements OnInit {
+  @Input() name: string;
+  @Input() color: string;
+
+  alpha: string =  "7a"
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
