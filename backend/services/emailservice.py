78571b2023b70221a74edabf5547ad87daaaf42d
Johannes <johannesr@viken.no>
2023-04-27 03:03:41 +0200

    added basic send email function

--- backend/services/emailservice.py
+++ backend/services/emailservice.py
@@ -1,5 +1,23 @@
+import smtplib, ssl
+
+tls_port = 587
+port = 465
+password = ""
+email_address = ""
+smtp_server = "smtp.gmail.com"
+
+context = ssl.create_default_context()
 
 
-def send_email(email: str, content: str = "") -> None:
-    ...
+def send_email(email: str, subject:str, content: str = "") -> None:
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(email_address, password)
+        server.sendmail(
+            email_address,
+            email,
+            f"Subject: {subject} \n" + content
+        )
+
+if __name__ == "__main__":
+    send_email("", "Hey!", "how you?")
 
