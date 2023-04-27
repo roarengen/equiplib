43c74dace1e69387d9465622680b36fc4e681ae8
Johannes <johannes>
2023-04-27 15:04:41 +0200

    templates bug in temps/by_org

--- backend/routes/templates.py
+++ backend/routes/templates.py
@@ -17,7 +17,7 @@ def get_temps(db: Session = Depends(get_db)):
 
 @api.get("/by_org/{orgid}", response_model=Template)
 def get_temp(orgid: int = require_user_to_be_in_org, db: Session = Depends(get_db)):
-    id = services.orgservice.get_org(db, orgid).organizationid
+    id = services.orgservice.get_org(db, orgid).id
     temp = crud.get_template(db, id)
     if not temp:
         return HTTPException(404, f"template not found with id: {id}")
