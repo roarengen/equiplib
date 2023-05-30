997e1023fc1f0bc29d437d9ad29b8547f42bba2a
Johannes <johannesr@viken.no>
2023-05-30 09:26:58 +0200

    template service now holds actual template ref
    redundancy in template service holds data that is already in the
    template, all references to these data should be redirected the
    template.*

--- frontend/equiplib/src/app/services/template.service.ts
+++ frontend/equiplib/src/app/services/template.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {Template} from '../models/template';
 import { AccountService } from './user.service';
 
 @Injectable({
@@ -9,11 +10,13 @@ export class TemplateService {
   public other1name?: string;
   public other2name?: string;
   public other3name?: string;
+  public template?: Template;
 
 
   constructor(
     public accountService: AccountService,
   ) {
+    this.template = accountService.template;
     this.other1name = accountService.template?.equipOther1;
     this.other2name = accountService.template?.equipOther2;
     this.other3name = accountService.template?.equipOther3;
