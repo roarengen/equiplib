98532bd119959ffc65cfa91f037f1e4825b9b3ef
Mads Thorstensen <mads.thorstensen@tietoevry.com>
2023-06-01 23:33:02 +0200

    Fixed scanner for testing.

--- frontend/equiplib/src/app/services/template.service.ts
+++ frontend/equiplib/src/app/services/template.service.ts
@@ -7,6 +7,9 @@ import { AccountService } from './user.service';
 })
 export class TemplateService {
 
+  public other1name?: string;
+  public other2name?: string;
+  public other3name?: string;
   public template?: Template;
 
 
@@ -14,5 +17,8 @@ export class TemplateService {
     public accountService: AccountService,
   ) {
     this.template = accountService.template;
+    this.other1name = accountService.template?.equipOther1;
+    this.other2name = accountService.template?.equipOther2;
+    this.other3name = accountService.template?.equipOther3;
 }
 }
