a06c057d6f9e48ed11c9dd18d37e5b0b4647e02c
Johannes <johannes>
2023-06-13 10:59:41 +0200

    refactored qr generation and service
    implemented new qr service and solution in manage-equipment page

--- frontend/equiplib/src/app/services/qr.service.ts
+++ frontend/equiplib/src/app/services/qr.service.ts
@@ -9,15 +9,26 @@ export class QrService {
     return `https://api.qrserver.com/v1/create-qr-code/?size=${size}x${size}&data=${data}`;
   }
 
-  downloadQrFromData(data: string, filename: string)
-  {
-    var url = QrService.formatUrl(data, this.size);
-    var link: HTMLAnchorElement = document.createElement('a');
-    link.href = url;
-    link.setAttribute('download', filename);
-    document.body.appendChild(link);
-    link.click();
-    window.URL.revokeObjectURL(url);
-    link.remove();
-  }
+  downloadQrFromData(data: string, filename: string) {
+  var url = QrService.formatUrl(data, this.size);
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'blob';
+
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      var blob = xhr.response;
+      var link: HTMLAnchorElement = document.createElement('a');
+      link.href = window.URL.createObjectURL(blob);
+      link.setAttribute('download', filename);
+      link.style.display = 'none';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  };
+
+  xhr.send();
+}
 }
